8538b686f2aa6eda321f54db8aa07de411d737ec
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-06-12 20:46:17 +0100

    feat: added user redirection in account layout

--- app/(account)/layout.tsx
+++ app/(account)/layout.tsx
@@ -1,4 +1,11 @@
-const Layout = ({ children }: { children: React.ReactNode }) => {
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await getServerSession();
+
+  if (session) redirect("/dashboard");
+
   return <main>{children}</main>;
 };
 
