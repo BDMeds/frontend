437d7a1875533d8277bb43952e4846ee5c9b41ab
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-06-13 21:41:17 +0100

    update: dashboard (overview) page layout

--- components/UI/Dashboard/Dashboard/index.tsx
+++ components/UI/Dashboard/Dashboard/index.tsx
@@ -3,14 +3,23 @@ import OverviewCard from "./card";
 
 const Dashboard = () => {
   return (
-    <div>
+    <div className="space-y-10">
       <div className="grid grid-cols-4 gap-6">
         {cards.map((card, id) => (
           <OverviewCard key={id} {...card} />
         ))}
       </div>
 
-      <div className="h-[140rem]"></div>
+      <div className="grid-cols-3 grid gap-5">
+        <div className="col-span-2 border border-white min-h-[20rem] rounded-lg bg-white"></div>
+        <div className="border border-white min-h-[20rem] rounded-lg bg-white"></div>
+      </div>
+
+      <div className="grid-cols-3 grid gap-5">
+        {Array.from({ length: 3 }).map((_, id) => (
+          <div className="border border-white min-h-[20rem] rounded-lg bg-white" key={id}></div>
+        ))}
+      </div>
     </div>
   );
 };
