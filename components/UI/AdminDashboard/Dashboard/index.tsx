8a1d443c6b2f81f26130953b418f68ddbefe00c1
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-06-17 09:18:55 +0100

    feat: added users bar chart

--- components/UI/AdminDashboard/Dashboard/index.tsx
+++ components/UI/AdminDashboard/Dashboard/index.tsx
@@ -1,11 +1,20 @@
 "use client";
 
 import AdminCards from "./cards";
+import UsersBarChart from "./charts/users-bar-chart";
 
 const AdminDashboard = () => {
   return (
-    <div className="space-y-4">
+    <div className="space-y-8">
       <AdminCards />
+
+      <div className="grid grid-cols-3">
+        <div className="bg-white col-span-2 p-4 border rounded-xl h-[30rem] space-y-5">
+          <p className="font-bold text-xl">Users</p>
+
+          <UsersBarChart />
+        </div>
+      </div>
     </div>
   );
 };
--- lib/helpers/numbers.ts
+++ lib/helpers/numbers.ts
@@ -20,15 +20,6 @@ export const convertDollarToNaira = (amount: number, rate: number) => amount * r
 
 export const convertNairaToDollar = (amount: number, rate: number) => amount / rate;
 
-export function formatSmallNumber(num: number, decimalPlaces: number) {
-  let numStr = num.toExponential(decimalPlaces);
-  let parts = numStr.split("e");
-  let coefficient = parseFloat(parts[0]);
-  let exponent = parseInt(parts[1], 10);
-  let result = (coefficient * Math.pow(10, exponent)).toFixed(decimalPlaces);
-  return result;
-}
-
 export function abbreviateBigNumbers(value: number) {
   if (value >= 1.0e9) {
     // Billions
