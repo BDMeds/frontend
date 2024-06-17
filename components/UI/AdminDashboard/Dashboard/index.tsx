b09f707b2c4ee7f45e9e85ebdd0cd991ae598aec
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-06-17 02:20:31 +0100

    feat: created admin cards

--- components/UI/AdminDashboard/Dashboard/index.tsx
+++ components/UI/AdminDashboard/Dashboard/index.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import AdminCards from "./cards";
+
 const AdminDashboard = () => {
-  return <div></div>;
+  return (
+    <div className="space-y-4">
+      <AdminCards />
+    </div>
+  );
 };
 
 export default AdminDashboard;
