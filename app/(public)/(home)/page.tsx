7ef58963c5b78193cffc6083aa4744cde6ff955e
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-06-18 14:51:18 +0100

    update: services basic anim

--- app/(public)/(home)/page.tsx
+++ app/(public)/(home)/page.tsx
@@ -3,14 +3,14 @@ import Departments from "@/components/UI/Home/departments";
 import Faq from "@/components/UI/Home/faq";
 import Hero from "@/components/UI/Home/hero";
 import Partners from "@/components/UI/Home/partners";
-import Services from "@/components/UI/Home/why-us";
+import WhyUs from "@/components/UI/Home/why-us";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <main className="space-y-32">
-        <Services />
+        <WhyUs />
         <Departments />
         <Appointment />
         <Partners />
--- components/UI/Home/why-us.tsx
+++ components/UI/Home/why-us.tsx
@@ -1,19 +1,35 @@
 "use client";
 
 import { services } from "@/lib/data/home";
+import { fadeToTopVariant, parentVariantSlow } from "@/lib/utils/variants";
+import { motion } from "framer-motion";
 
-const Services = () => {
+const WhyUs = () => {
   return (
     <section>
       <div className="container my-20">
-        <div className="grid md:grid-cols-3 sm:grid-cols-2 lg:gap-10 gap-6 justify-center flex-wrap">
-          <div className="space-y-3 p-5">
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1, transition: { staggerChildren: 0.1 } }}
+          viewport={{ once: true }}
+          className="grid md:grid-cols-3 sm:grid-cols-2 lg:gap-10 gap-6 justify-center flex-wrap"
+        >
+          <motion.div
+            variants={fadeToTopVariant}
+            initial="initial"
+            animate="animate"
+            key="434"
+            className="space-y-3 p-5"
+          >
             <p className="text-lg">Services</p>
             <p className="font-extrabold text-4xl text-primary">Provides Our Best Services</p>
-          </div>
+          </motion.div>
           {services.map(({ description, icon, title }, id) => (
-            <div
+            <motion.div
               key={id}
+              variants={fadeToTopVariant}
+              initial="initial"
+              animate="animate"
               className="px-5 py-7 space-y-4 bg-white shadow-lg shadow-gray-100 rounded-xl duration-300 hover:shadow-2xl"
             >
               <div className="size-12 border rounded-full bg-primary text-white grid place-content-center">{icon}</div>
@@ -21,12 +37,12 @@ const Services = () => {
               <p className={`font-extrabold text-2xl`}>{title}</p>
 
               <p className="text-gray-500">{description}</p>
-            </div>
+            </motion.div>
           ))}
-        </div>
+        </motion.div>
       </div>
     </section>
   );
 };
 
-export default Services;
+export default WhyUs;
