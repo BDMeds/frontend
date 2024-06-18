78dc49177d6c60b9916f9054792cc036da60607f
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-06-18 17:38:51 +0100

    feat: added chart for heart

--- components/UI/Dashboard/Reports/details/index.tsx
+++ components/UI/Dashboard/Reports/details/index.tsx
@@ -1,13 +1,32 @@
 "use client";
 
-import HeartModel from "@/components/Common/3D/heart";
 import { parentVariant } from "@/lib/utils/variants";
 import { motion } from "framer-motion";
+import { DumChart2, DummyChart1 } from "./charts";
+import Loader from "@/components/Common/Loaders";
+import dynamic from "next/dynamic";
+import BloodPressureBarChart from "./charts/blood-pressure";
+import HeartRateLineChart from "./charts/heart-rate-line";
+
+const HeartModel = dynamic(() => import("@/components/Common/3D/heart"), {
+  ssr: false,
+  loading: () => (
+    <div className="grid place-content-center min-h-[10rem] text-center">
+      <div className="space-y-2">
+        <div className="grid place-content-center">
+          <Loader />
+        </div>
+
+        <p className="text-sm">Calendar loading...</p>
+      </div>
+    </div>
+  ),
+});
 
 const ReportsDetails = () => {
   return (
     <div className="grid grid-cols-2 gap-7 pb-8">
-      <div className="border rounded-xl bg-white relative">
+      <div className="border rounded-xl bg-white relative self-start">
         <div className="absolute top-2 p-5 left-0 space-y-1">
           <p>Evaluation:</p>
           <p className="text-4xl font-bold">Overall State of Health</p>
@@ -17,17 +36,26 @@ const ReportsDetails = () => {
       </div>
 
       <div className="grid grid-rows-7 space-y-7">
-        <motion.div {...parentVariant} animate="animate" className="grid grid-cols-3 gap-5 row-span-2">
-          {Array.from({ length: 3 }).map((_, id) => (
-            <div className="rounded-xl bg-white border p-14" key={id}></div>
-          ))}
+        <motion.div {...parentVariant} animate="animate" className="grid gap-3 row-span-2">
+          <div className="rounded-xl bg-white border py-4 space-y-5">
+            <p className="font-bold px-5">Blood Pressure</p>
+            <div className="h-[15rem]">
+              <BloodPressureBarChart />
+            </div>
+          </div>
+          <div className="rounded-xl bg-white border py-4 space-y-5">
+            <p className="font-bold px-5">Heart Rate</p>
+            <div className="h-[15rem]">
+              <HeartRateLineChart />
+            </div>
+          </div>
         </motion.div>
 
-        <motion.div {...parentVariant} animate="animate" className="grid grid-cols-2 gap-5 row-span-5">
+        {/* <motion.div {...parentVariant} animate="animate" className="grid grid-cols-2 gap-5 row-span-5">
           {Array.from({ length: 2 }).map((_, id) => (
             <div className="rounded-xl bg-white border min-h-[18rem]" key={id}></div>
           ))}
-        </motion.div>
+        </motion.div> */}
       </div>
     </div>
   );
