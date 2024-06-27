30fdcc92f4a4625a4ef5a6b8209e680de1fe0aec
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-06-27 19:38:35 +0100

    Update index.tsx

--- lib/providers/index.tsx
+++ lib/providers/index.tsx
@@ -19,11 +19,24 @@ const Providers = ({ children }: { children: ReactNode }) => {
     updateDarkMode(darkModeMediaQuery.matches);
 
     const darkModeListener = (event: MediaQueryListEvent) => {
-      // if (event.matches) {
-      //   document.body.classList.add("dark");
+      // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+      // if (
+      //   localStorage.theme === "dark" ||
+      //   (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+      // ) {
+      //   document.documentElement.classList.add("dark");
       // } else {
-      //   document.body.classList.remove("dark");
+      //   document.documentElement.classList.remove("dark");
       // }
+
+      // // Whenever the user explicitly chooses light mode
+      // localStorage.theme = "light";
+
+      // // Whenever the user explicitly chooses dark mode
+      // localStorage.theme = "dark";
+
+      // // Whenever the user explicitly chooses to respect the OS preference
+      // localStorage.removeItem("theme");
 
       updateDarkMode(event.matches);
     };
